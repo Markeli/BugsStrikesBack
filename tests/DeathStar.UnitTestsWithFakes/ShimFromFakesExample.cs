33233603d0eeec99fed1a23c4826afad39b3260e
Maxim Markelow <maxim.markelow@siisltd.ru>
2019-01-23 20:26:32 +0300

    Main lib migrated to full framework; added fake using examples

---
+++ tests/DeathStar.UnitTestsWithFakes/ShimFromFakesExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Fakes;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeathStar.UnitTestsWithFakes
+{
+    /// <summary>
+    /// Пример создания подделки для статического класса
+    /// </summary>
+    [TestClass]
+    public class ShimFromFakesExample
+    {
+        [TestMethod]
+        public void ShimTest()
+        {
+            // 1. создаем ShimContext - это обязательно
+            using (ShimsContext.Create())
+            {
+                // 2. задаем нужное поведение
+                // нюанс: к имени объекта добавляется префикс Shim
+                ShimDateTime.NowGet = () => new DateTime(2000, 1, 1);
+
+                // 3. а тут тестируем то, что нам надо
+            }
+        }
+    }
+}
