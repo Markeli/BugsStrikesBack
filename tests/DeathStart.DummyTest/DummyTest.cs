ba69ce5255804e803a72a11c837a4ae261445e0f
Maxim Markelow <maxim.markelow@siisltd.ru>
2019-01-23 19:50:32 +0300

    Added dummy test

---
+++ tests/DeathStart.DummyTest/DummyTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DeathStar.DummyTest
+{
+    class DummyTest
+    {
+        static async Task Main(string[] args)
+        {
+            await TestReactorAsync();
+
+            Console.ReadKey();
+        }
+
+        private static async Task TestReactorAsync()
+        {
+            var reactor = new Reactor(100);
+            await reactor.ActivateAsync();
+
+            var energy = reactor.GetEnergy(100);
+
+            Console.WriteLine(energy != null);
+            Console.WriteLine(energy.Level == 100);
+        }
+    }
+}
